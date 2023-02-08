08371dd50f960dbfb85595cb8700bf9f501e89f4
toño aguirre <caaz160782@gmail.com>
2023-02-08 17:23:04 -0600

    install socket io

--- models/server.js
+++ models/server.js
@@ -6,10 +6,12 @@ require("dotenv").config();
 class Server {
 
     constructor() {
-        this.app  = express();
-        this.port = process.env.PORT;
-        this.paths = {
-            }
+        this.app    = express();
+        this.port   = process.env.PORT;
+        this.server = require('http').createServer(this.app);
+        this.io = require('socket.io')(this.server);
+        //io.on('connection', () => { /* … */ });
+        this.paths = { }
      
         // Middlewares
         this.middlewares();
@@ -33,7 +35,7 @@ class Server {
             }
 
     listen() {
-        this.app.listen( this.port, () => {
+        this.server.listen( this.port, () => {
             console.log('🚀 Servidor corriendo en puerto 🚀', this.port );
         });
     }
